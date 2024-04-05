a4bc88d05e43a24366e8fb525ea9476a62d53763
Joel Wong <joelwong@Joels-MacBook-Pro.local>
2024-04-06 02:23:01 +0800

    fix: images not showing

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,42 +1,62 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Image from 'next/image';
+import Image from "next/image";
 
 const About = () => {
   return (
     <main className="flex min-h-screen flex-col bg-[#FFFBFA]">
-    <Navbar/>
+      <Navbar />
 
+      <div className="container mt-36 mx-auto py-4 px-12">
+        <div className="grid grid-cols-1 lg:grid-cols-12">
+          <div className="col-span-7 place-self-left pb-16">
+            <h1 className="text-[#574F4A] text-2xl font-bold pb-8">
+              Hello there, 👋🏻
+            </h1>
 
-    <div className="container mt-36 mx-auto py-4 px-12">
-    <div className="grid grid-cols-1 lg:grid-cols-12">
-      <div className='col-span-7 place-self-left pb-16'>
-        <h1 className='text-[#574F4A] text-2xl font-bold pb-8'> 
-          Hello there,  👋🏻</h1>
+            <p className="text-[#574F4A] pb-4 leading-relaxed">
+              I’m Melanie. I’m a fresh graduate from the National University of
+              Singapore having studied a focus on{" "}
+              <b>
+                Interactive Media Development (UI/UX) and Architectural Studies
+              </b>
+              .{" "}
+            </p>
+            <p className="text-[#574F4A] pb-4 leading-relaxed">
+              {" "}
+              A little fun fact about me: I studied Architecture in my first
+              three semesters of university! My architecture school experience
+              continues to guide my design thinking processes. I seek to make
+              informed, sensible design decisions that empowers users.{" "}
+            </p>
+            <p className="text-[#574F4A] pb-4 leading-relaxed">
+              I have some experience in software engineering, having assisted
+              with front-end development for my recent internship as a{" "}
+              <b>Digital Innovation Intern at Singapore Airlines</b>.
+              Previously, I’ve been a{" "}
+              <b>UI/UX intern at Activate Interactive</b>, as well as a{" "}
+              <b>Undergraduate Teaching Assistant for Interaction Design</b>.{" "}
+            </p>
+          </div>
 
-        <p className='text-[#574F4A] pb-4 leading-relaxed'>I’m Melanie. I’m a fresh graduate from the National University of Singapore having studied a focus on <b>Interactive Media Development (UI/UX) and Architectural Studies</b>. </p>
-        <p className='text-[#574F4A] pb-4 leading-relaxed'> A little fun fact about me: I studied Architecture in my first three semesters of university! My architecture school experience continues to guide my design thinking processes. I seek to make informed, sensible design decisions that empowers users. </p> 
-        <p className='text-[#574F4A] pb-4 leading-relaxed'>I have some experience in software engineering, having assisted with front-end development for my recent internship as a <b>Digital Innovation Intern at Singapore Airlines</b>. Previously, I’ve been a <b>UI/UX intern at Activate Interactive</b>, as well as a <b>Undergraduate Teaching Assistant for Interaction Design</b>. </p>
+          <div className="col-span-5  place-self-right lg:pl-16">
+            <Image
+              src="/images/melpic.jpg"
+              alt="Melanie's Picture"
+              width={302.4}
+              height={403.2}
+              className="rounded-xl"
+            />
+          </div>
+        </div>
       </div>
 
-      <div className='col-span-5  place-self-right lg:pl-16'>
-        <Image src="/images/melpic.jpg" alt="Melanie's Picture" width={302.4} height={403.2} className='rounded-xl'/>
+      <div className="container mt-24 mx-auto py-4 px-12">
+        <Footer />
       </div>
-    
-    </div>
-    </div>
-    
-    
-    <div className="container mt-24 mx-auto py-4 px-12">
-      <Footer/>
-    </div>
+    </main>
+  );
+};
 
-    
-  </main>
-  )
-}
-
-export default About
-
-
+export default About;
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,50 +1,55 @@
-"use client"
-import React from 'react'
-import { useRouter, usePathname} from 'next/navigation';
-import Link from 'next/link';
+"use client";
+import React from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
-
 const Footer = () => {
+  const currentPage = usePathname();
 
-  const currentPage = usePathname()
-
-  let secondHeadingText = '';
-  let secondHeadingUrl ="";
+  let secondHeadingText = "";
+  let secondHeadingUrl = "";
 
   switch (currentPage) {
-    case '/about':
-      secondHeadingText = 'See my design works 🎨';
-      secondHeadingUrl="/#works"
+    case "/about":
+      secondHeadingText = "See my design works 🎨";
+      secondHeadingUrl = "/#works";
       break;
     default:
-      secondHeadingText = 'Learn more about me 👀';
-      secondHeadingUrl="/about"
-    }
-
+      secondHeadingText = "Learn more about me 👀";
+      secondHeadingUrl = "/about";
+  }
 
   return (
-    <div className='bottom-0 left-0 right-0 mt-16'>
-        <div className='flex flex-col items-left mx-auto  pb-32'>
-            <h3 className='text-[#76655B] text-lg font-bold py-2'>... that's not all!</h3>
+    <div className="bottom-0 left-0 right-0 mt-16">
+      <div className="flex flex-col items-left mx-auto  pb-32">
+        <h3 className="text-[#76655B] text-lg font-bold py-2">
+          ... that&apos;s not all!
+        </h3>
 
-            <Link href={secondHeadingUrl}>
-            <h3 className='flex items-center text-[#574F4A] text-2xl hover:text-[#574F4A] hover:text-opacity-50 font-bold py-2'>
-              {secondHeadingText}<ArrowRightIcon className='ml-3 h-5 w-5'/></h3>
-            </Link>
+        <Link href={secondHeadingUrl}>
+          <h3 className="flex items-center text-[#574F4A] text-2xl hover:text-[#574F4A] hover:text-opacity-50 font-bold py-2">
+            {secondHeadingText}
+            <ArrowRightIcon className="ml-3 h-5 w-5" />
+          </h3>
+        </Link>
 
-            <Link href="">
-            <h3 className='flex items-center text-[#574F4A] text-2xl hover:text-[#574F4A] hover:text-opacity-50 font-bold py-2'>
-              view my resume 📄<ArrowRightIcon className='ml-3 h-5 w-5'/></h3>
-            </Link>
+        <Link href="">
+          <h3 className="flex items-center text-[#574F4A] text-2xl hover:text-[#574F4A] hover:text-opacity-50 font-bold py-2">
+            view my resume 📄
+            <ArrowRightIcon className="ml-3 h-5 w-5" />
+          </h3>
+        </Link>
 
-            <Link href="https://www.linkedin.com/in/melaniequek">
-              <h3 className='flex items-center text-[#574F4A] text-2xl hover:text-[#574F4A] hover:text-opacity-50 font-bold py-2'>
-                or say hello on Linkedin 👋🏻<ArrowRightIcon className='ml-3 h-5 w-5'/></h3>
-            </Link>
-        </div>
+        <Link href="https://www.linkedin.com/in/melaniequek">
+          <h3 className="flex items-center text-[#574F4A] text-2xl hover:text-[#574F4A] hover:text-opacity-50 font-bold py-2">
+            or say hello on Linkedin 👋🏻
+            <ArrowRightIcon className="ml-3 h-5 w-5" />
+          </h3>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
